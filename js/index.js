2222ac7657511c387f6d25c4b9b4ff47422a2698
Jacob Kirby <65231998+Kirby5253@users.noreply.github.com>
2020-08-11 19:49:24 -0500

    added call button and contact form

--- js/index.js
+++ js/index.js
@@ -3,3 +3,21 @@ $('.navbar-toggler').click(function(e) {
   $('.nav-box').slideToggle(300);
   return;
 });
+
+$(window).resize(function() {
+  navBoxBreakpoint();
+});
+
+navBoxBreakpoint = () => {
+  if ($(window).width() > 767) {
+    $('.nav-box').hide();
+  } else if ($(window).width() <= 767 && $('.navbar-collapse').hasClass('show')) {
+    $('.nav-box').show();
+  } else {
+    $('.nav-box').hide();
+  }
+};
+
+$(window).load(function() {
+  $('body').fadeIn();
+});
