b1c2edafcb09920adb1ba30fce0a103bb408cb46
Jacob Kirby <65231998+Kirby5253@users.noreply.github.com>
2020-08-12 15:36:08 -0500

    connected contact to formspree

--- js/index.js
+++ js/index.js
@@ -18,6 +18,73 @@ navBoxBreakpoint = () => {
   }
 };
 
-$(window).load(function() {
+var submitted = false;
+$(document).ready(function() {
   $('body').fadeIn();
+
+  $('#fs-frm').on('submit', function(e) {
+    e.preventDefault();
+
+    //get the name field
+    var name = $('#contact-name').val();
+    //get the name field value
+    var email = $('#contact-email').val();
+    //get the comments
+    var message = $('#contact-message').val();
+
+    $.ajax({
+      url: 'https://formspree.io/xknqevlg',
+      method: 'POST',
+      data: {
+        name: name,
+        email: email,
+        message: message,
+        _subject: 'Form Submission',
+      },
+      dataType: 'json',
+      success: function() {
+        console.log('success');
+      },
+    });
+  });
 });
+
+// function validateForm() {
+//   let name = $('#contact-name');
+//   let email = $('#contact-email');
+//   let message = $('#contact-message');
+//   let submitButton = $('#sendMessage');
+
+//   name && email && message ? submitButton.removeClass('disabled') : null;
+// }
+
+// handles contact form submission
+// var submitted = false;
+// $('#fs-frm').on('submit', function(e) {
+//   e.preventDefault();
+//   $('#fs-frm *').slideUp(600, function() {
+//     $(this).remove();
+//   });
+//   $('#fs-frm').prepend("Thanks for the submission! I'll get back to you as soon as possible.");
+// });
+
+// var message = '';
+
+// $('#sendMessage').on('click', function() {
+//   message = $('#fs-frm').serialize().split('&');
+//   $.ajax({
+//     url: 'https://formspree.io/xknqevlg',
+//     method: 'POST',
+//     data: { message: message },
+//     dataType: 'json',
+//   });
+// });
+
+function validateForm() {
+  let name = $('#contact-name');
+  let email = $('#contact-email');
+  let message = $('#contact-message');
+  let submitButton = $('#sendMessage');
+
+  name && email && message ? submitButton.removeClass('disabled') : null;
+}
