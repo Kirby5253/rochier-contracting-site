59956ba62cd461c02a20ecf4521c3a6f360595b7
Jacob Kirby <65231998+Kirby5253@users.noreply.github.com>
2020-08-13 10:27:14 -0500

    fixed form not sending

--- js/index.js
+++ js/index.js
@@ -22,63 +22,32 @@ var submitted = false;
 $(document).ready(function() {
   $('body').fadeIn();
 
-  $('#fs-frm').on('submit', function(e) {
-    e.preventDefault();
-
-    //get the name field
-    var name = $('#contact-name').val();
-    //get the name field value
-    var email = $('#contact-email').val();
-    //get the comments
-    var message = $('#contact-message').val();
-
-    $.ajax({
-      url: 'https://formspree.io/xknqevlg',
-      method: 'POST',
-      data: {
-        name: name,
-        email: email,
-        message: message,
-        _subject: 'Form Submission',
-      },
-      dataType: 'json',
-      success: function() {
-        console.log('success');
-      },
-    });
+  $.ajaxSetup({
+    cache: false,
   });
 });
 
-// function validateForm() {
-//   let name = $('#contact-name');
-//   let email = $('#contact-email');
-//   let message = $('#contact-message');
-//   let submitButton = $('#sendMessage');
-
-//   name && email && message ? submitButton.removeClass('disabled') : null;
-// }
-
-// handles contact form submission
-// var submitted = false;
-// $('#fs-frm').on('submit', function(e) {
-//   e.preventDefault();
-//   $('#fs-frm *').slideUp(600, function() {
-//     $(this).remove();
-//   });
-//   $('#fs-frm').prepend("Thanks for the submission! I'll get back to you as soon as possible.");
-// });
-
-// var message = '';
-
-// $('#sendMessage').on('click', function() {
-//   message = $('#fs-frm').serialize().split('&');
-//   $.ajax({
-//     url: 'https://formspree.io/xknqevlg',
-//     method: 'POST',
-//     data: { message: message },
-//     dataType: 'json',
-//   });
-// });
+var $contactForm = $('#fs-frm');
+$contactForm.submit(function(e) {
+  $.ajax({
+    cache: false,
+    url: '//formspree.io/xknqevlg',
+    method: 'POST',
+    data: $(this).serialize(),
+    dataType: 'json',
+    beforeSend: function() {
+      $contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+    },
+    success: function(data) {
+      $contactForm.find('.alert--loading').hide();
+      $contactForm.append('<div class="alert alert--success">Message sent!</div>');
+    },
+    error: function(err) {
+      $contactForm.find('.alert--loading').hide();
+      $contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+    },
+  });
+});
 
 function validateForm() {
   let name = $('#contact-name');
