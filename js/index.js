d5221f08302ca0a65b64cd3287915c4ca558d486
Jacob Kirby <65231998+Kirby5253@users.noreply.github.com>
2020-08-19 22:37:41 -0500

    deleted outdated files and fixed carousel bug

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
-$(window).resize(function () {
-	navBoxBreakpoint();
+// Fixed the nav menu not closing on selection
+$('.nav-item').on('click', function() {
+  $('.navbar-collapse').removeClass('show');
 });
 
-$(".nav-item").on("click", function () {
-	$(".navbar-collapse").removeClass("show");
+// Scrolling animation for navbar
+$(function() {
+  $('ul.navbar-nav a').bind('click', function(event) {
+    event.preventDefault();
+    var $anchor = $(this);
+    console.log($anchor.attr('href'));
+    var scrollAnimation = $($anchor.attr('href')).offset();
+    $('html, body').stop().animate(
+      {
+        scrollTop: scrollAnimation.top,
+      },
+      1000,
+    );
+    event.preventDefault();
+  });
 });
